a1c609550bd5c070919f61bfbede1ed8bd3ff9b5
Oh <htcvtc59@gmail.com>
2019-07-12 11:32:12 +0700

    images

--- src/main/java/com/demo/response/DataResponse.java
+++ src/main/java/com/demo/response/DataResponse.java
@@ -14,15 +14,63 @@ public class DataResponse {
     @JsonProperty("data")
     private Object data;
     private DataResponse.DataType dataType = DataResponse.DataType.NORMAL;
-    private boolean isEscape = true;
 
 
-    public DataResponse(String errorCode, String errorMessage, Object data, DataType dataType, boolean isEscape) {
+    public DataResponse(String error, String message) {
+        this.errorCode = error;
+        this.errorMessage = message;
+    }
+
+    public DataResponse(String error, String message, String data) {
+        this.errorCode = error;
+        this.errorMessage = message;
+        this.data = data;
+    }
+
+    public DataResponse(Object data) {
+        this.errorCode = ResponseCode.SUCCESSFUL;
+        this.errorMessage = "";
+        this.data = data;
+    }
+
+    public DataResponse(String errorCode, String errorMessage, Object data, DataType dataType) {
         this.errorCode = errorCode;
         this.errorMessage = errorMessage;
         this.data = data;
         this.dataType = dataType;
-        this.isEscape = isEscape;
+    }
+
+    @JsonIgnore
+    public String getError() {
+        return this.errorCode;
+    }
+
+    @JsonIgnore
+    public String getMessage() {
+        return this.errorMessage;
+    }
+
+    @JsonIgnore
+    public Object getData() {
+        return this.data;
+    }
+
+    public void setData(Object data) {
+        this.setData(data, DataResponse.DataType.NORMAL);
+    }
+
+    public void setData(Object data, DataResponse.DataType dataType) {
+        this.data = data;
+        this.dataType = dataType;
+    }
+
+    @JsonIgnore
+    public DataType getDataType() {
+        return dataType;
+    }
+
+    public void setDataType(DataType dataType) {
+        this.dataType = dataType;
     }
 
     @Override
@@ -30,26 +78,26 @@ public class DataResponse {
         return DataResponse.toJsonString(this);
     }
 
-    private static String toJsonString(DataResponse dataResponse) {
+    public static final DataResponse SUCCESSFUL = new DataResponse(ResponseCode.SUCCESSFUL, "SUCCESSFUL");
+    public static final DataResponse FAILED = new DataResponse(ResponseCode.FAILED, "FAILED");
+
+    public static String toJsonString(DataResponse dataResponse) {
         try {
-
             ObjectMapper mapper = null;
-            if (dataResponse.isEscape){
 
+            if (dataResponse.getDataType() == DataResponse.DataType.JSON_STR) {
+                return "{\"error_code\":\"" + dataResponse.getError() + "\",\"error_message\":\"" + dataResponse.getMessage() + "\", \"data\":" + dataResponse.getData() + "}";
             }
-
-        } catch (Exception ex) {
-
+            // DATA_TYPE_NORMAL
+            return mapper.writeValueAsString(dataResponse);
+        } catch (Exception e) {
         }
+
         return "";
-
     }
-
 
     public enum DataType {
         NORMAL, JSON_STR, UNSURE
     }
-
-    ;
 
 }
