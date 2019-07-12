141a67a1b84b54bda48aca4a2adc9660b91c8a5f
pewdew <090297anhduc@gmail.com>
2019-07-12 16:12:32 +0700

    no message

--- src/main/java/com/demo/controller/HomeController.java
+++ src/main/java/com/demo/controller/HomeController.java
@@ -1,13 +1,16 @@
 package com.demo.controller;
 
 
-import org.springframework.stereotype.Controller;
+import com.demo.model.LoginModel;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.validation.Valid;
 import java.text.DateFormat;
 import java.util.Date;
 import java.util.Locale;
@@ -20,8 +23,17 @@ public class HomeController {
         Date date = new Date();
         DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
         String formattedDate = dateFormat.format(date);
-        ModelAndView mav = new ModelAndView("index");
+        ModelAndView mav = new ModelAndView("login");
         mav.addObject("serverTime", formattedDate);
+        mav.addObject("loginModel", new LoginModel());
+        return mav;
+    }
+    @RequestMapping(value = {"/login"}, method = RequestMethod.POST)
+    private ModelAndView login(@ModelAttribute LoginModel loginModel){
+        String password = loginModel.getPassword();
+        loginModel.getUsername();
+
+        ModelAndView mav = new ModelAndView("home");
         return mav;
     }
 
--- src/main/java/com/demo/transport/API.java
+++ src/main/java/com/demo/transport/API.java
@@ -1,64 +1,64 @@
-package com.demo.transport;
-
-import com.demo.util.Encryption;
-import org.apache.http.HttpResponse;
-import org.apache.http.client.ClientProtocolException;
-import org.apache.http.client.HttpClient;
-import org.apache.http.client.methods.HttpPost;
-import org.apache.http.entity.StringEntity;
-import org.apache.http.impl.client.HttpClientBuilder;
-import org.json.simple.JSONObject;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.*;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.UnsupportedEncodingException;
-
-@RestController
-@RequestMapping(path = "/auth")
-public class API {
-
-    @Value("${app.api.login}")
-    private String API_LOGIN;
-    @Value("${app.integrationkey}")
-    private String INTEGRATION_KEY;
-    @Value("${app.secretkey}")
-    private String SECRET_KEY;
-
-
-    @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
-    private JSONObject authentication(@RequestBody JSONObject auth) {
-        try {
-            String username = (String) auth.get("username");
-            String password = (String) auth.get("password");
-            String unixTimeStamp = String.valueOf(System.currentTimeMillis() / 1000L);
-            String hmacData = username + password + INTEGRATION_KEY + unixTimeStamp;
-            String hmac = Encryption.encrypt(SECRET_KEY, hmacData);
-            HttpClient client = HttpClientBuilder.create().build();
-            HttpPost post = new HttpPost(API_LOGIN);
-            JSONObject data = new JSONObject();
-            data.put("username", username);
-            data.put("password", password);
-            data.put("integrationKey", INTEGRATION_KEY);
-            data.put("unixTimestamp", unixTimeStamp);
-            data.put("hmac", hmac);
-
-            HttpClientHelper.getInstance().sendTo(API_LOGIN,)
-
-            System.out.println(result);
-
-            return null;
-        } catch (UnsupportedEncodingException e) {
-            e.printStackTrace();
-        } catch (ClientProtocolException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        return new JSONObject();
-    }
-
-
-}
+//package com.demo.transport;
+//
+//import com.demo.util.Encryption;
+//import org.apache.http.HttpResponse;
+//import org.apache.http.client.ClientProtocolException;
+//import org.apache.http.client.HttpClient;
+//import org.apache.http.client.methods.HttpPost;
+//import org.apache.http.entity.StringEntity;
+//import org.apache.http.impl.client.HttpClientBuilder;
+//import org.json.simple.JSONObject;
+//import org.springframework.beans.factory.annotation.Value;
+//import org.springframework.web.bind.annotation.*;
+//
+//import java.io.BufferedReader;
+//import java.io.IOException;
+//import java.io.InputStreamReader;
+//import java.io.UnsupportedEncodingException;
+//
+//@RestController
+//@RequestMapping(path = "/auth")
+//public class API {
+//
+//    @Value("${app.api.login}")
+//    private String API_LOGIN;
+//    @Value("${app.integrationkey}")
+//    private String INTEGRATION_KEY;
+//    @Value("${app.secretkey}")
+//    private String SECRET_KEY;
+//
+//
+//    @PostMapping(path = "/login", consumes = "application/json", produces = "application/json")
+//    private JSONObject authentication(@RequestBody JSONObject auth) {
+//        try {
+//            String username = (String) auth.get("username");
+//            String password = (String) auth.get("password");
+//            String unixTimeStamp = String.valueOf(System.currentTimeMillis() / 1000L);
+//            String hmacData = username + password + INTEGRATION_KEY + unixTimeStamp;
+//            String hmac = Encryption.encrypt(SECRET_KEY, hmacData);
+//            HttpClient client = HttpClientBuilder.create().build();
+//            HttpPost post = new HttpPost(API_LOGIN);
+//            JSONObject data = new JSONObject();
+//            data.put("username", username);
+//            data.put("password", password);
+//            data.put("integrationKey", INTEGRATION_KEY);
+//            data.put("unixTimestamp", unixTimeStamp);
+//            data.put("hmac", hmac);
+//
+////            HttpClientHelper.getInstance().sendTo(API_LOGIN,)
+//
+////            System.out.println(result);
+//
+//            return null;
+//        } catch (UnsupportedEncodingException e) {
+//            e.printStackTrace();
+//        } catch (ClientProtocolException e) {
+//            e.printStackTrace();
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//        return new JSONObject();
+//    }
+//
+//
+//}
